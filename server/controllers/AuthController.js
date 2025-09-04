2025880e2879c6fe009f5aaeaad2e36984a52428
panjyar <panjiyarkehuli@gmail.com>
2025-09-04 21:33:47 +0530

    feat: JWT Token Verification & Middleware

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -86,19 +86,22 @@ try {
 export const getUserInfo = async (request, response, next) => {
 try {
   console.log(request.userId);
-    // return response.status(200).json({
-    //   user: {
-    //     id: user.id,
-    //     email: user.email,
-    //     profileSetup: user.profileSetup,
-    //     firstName: user.firstName,
-    //     lastName: user.lastName,
-    //     image: user.image,
-    //     color: user.color,
-    //   },
-    // });
-  } catch (error) {
-    console.log(error);
-    return response.status(500).send({ message: "Internal Server Error" });
+  const userData = await User.findById(request.userId);
+  if(!userData) {
+    return response.status(404).send("User not found");
   }
+  return response.status(200).json({
+      id: userData.id,
+      email: userData.email,
+      profileSetup: userData.profileSetup,
+      firstName: userData.firstName,
+      lastName: userData.lastName,
+      image: userData.image,
+      color: userData.color,
+  
+  });
+} catch (error) {
+  console.log(error);
+  return response.status(500).send({ message: "Internal Server Error" });
+}
 };
